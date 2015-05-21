d0fbec7edc29fb10d9a807edf788f8777578a2b6
Andreas Niedermair <andreas@niedermair.name>
2015-05-21 21:11:51 +0200

    removed demo service and request, fixed build

--- Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.Tests/UnitTest1.cs
+++ Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.Tests/UnitTest1.cs
@@ -1,43 +1,42 @@
-﻿using System;
+﻿using DoeInc.Ticketing.ServiceInterface;
 using NUnit.Framework;
-using DoeInc.Ticketing.ServiceInterface;
-using DoeInc.Ticketing.ServiceModel;
-using ServiceStack.Testing;
 using ServiceStack;
+using ServiceStack.Testing;
 
 namespace DoeInc.Ticketing.Tests
 {
     [TestFixture]
     public class UnitTests
     {
-        private readonly ServiceStackHost appHost;
+        private readonly ServiceStackHost _appHost;
 
         public UnitTests()
         {
-            appHost = new BasicAppHost(typeof(MyServices).Assembly)
-            {
-                ConfigureContainer = container =>
-                {
-                    //Add your IoC dependencies here
-                }
-            }
-            .Init();
+            this._appHost = new BasicAppHost(typeof (TicketService).Assembly)
+                            {
+                                ConfigureContainer = container =>
+                                                     {
+                                                         //Add your IoC dependencies here
+                                                     }
+                            }.Init();
         }
 
         [TestFixtureTearDown]
         public void TestFixtureTearDown()
         {
-            appHost.Dispose();
+            this._appHost.Dispose();
         }
 
         [Test]
         public void TestMethod1()
         {
+            /*
             var service = appHost.Container.Resolve<MyServices>();
 
             var response = (HelloResponse)service.Any(new Hello { Name = "World" });
 
             Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+            */
         }
     }
 }
--- Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.Web/AppHost.cs
+++ Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.Web/AppHost.cs
@@ -7,18 +7,18 @@ namespace DoeInc.Ticketing.Web
     public class AppHost : AppHostBase
     {
         /// <summary>
-        /// Default constructor.
-        /// Base constructor requires a name and assembly to locate web service classes. 
+        ///     Default constructor.
+        ///     Base constructor requires a name and assembly to locate web service classes.
         /// </summary>
         public AppHost()
-            : base("DoeInc.Ticketing", typeof(MyServices).Assembly)
+            : base("DoeInc.Ticketing",
+                   typeof (TicketService).Assembly)
         {
-
         }
 
         /// <summary>
-        /// Application specific configuration
-        /// This method should initialize any IoC resources utilized by your web service classes.
+        ///     Application specific configuration
+        ///     This method should initialize any IoC resources utilized by your web service classes.
         /// </summary>
         /// <param name="container"></param>
         public override void Configure(Container container)
