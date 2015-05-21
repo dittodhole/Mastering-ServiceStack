7b054357319d5e1c34f92cf57314b44e6987fee6
Andreas Niedermair <andreas@niedermair.name>
2015-05-21 20:19:46 +0200

    initial commit of ticketing

---
+++ Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.Web/AppHost.cs
@@ -0,0 +1,31 @@
+﻿using DoeInc.Ticketing.ServiceInterface;
+using Funq;
+using ServiceStack;
+
+namespace DoeInc.Ticketing.Web
+{
+    public class AppHost : AppHostBase
+    {
+        /// <summary>
+        /// Default constructor.
+        /// Base constructor requires a name and assembly to locate web service classes. 
+        /// </summary>
+        public AppHost()
+            : base("DoeInc.Ticketing", typeof(MyServices).Assembly)
+        {
+
+        }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        /// <param name="container"></param>
+        public override void Configure(Container container)
+        {
+            //Config examples
+            //this.Plugins.Add(new PostmanFeature());
+            //this.Plugins.Add(new CorsFeature());
+        }
+    }
+}
