4423f9f19b935ec36a9ee344399cd0dd665d8331
Andreas Niedermair <andreas@niedermair.name>
2015-05-21 20:14:48 +0200

    added Tasker demo

---
+++ Chapter 02/DoeInc.Tasker.Web/DoeInc.Tasker.Web/ServiceStackPageHandlerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.UI;
+using ServiceStack;
+
+namespace DoeInc.Tasker.Web
+{
+    public class ServiceStackPageHandlerFactory : PageHandlerFactory
+    {
+        public override IHttpHandler GetHandler(HttpContext context,
+                                                string requestType,
+                                                string virtualPath,
+                                                string path)
+        {
+            var httpHandler = base.GetHandler(context,
+                                              requestType,
+                                              virtualPath,
+                                              path);
+
+            ServiceStackHost.Instance.GetContainer()
+                            .AutoWire(httpHandler);
+
+            return httpHandler;
+        }
+    }
+}
