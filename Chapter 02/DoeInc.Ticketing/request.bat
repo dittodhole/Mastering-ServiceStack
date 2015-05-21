f24dc5c8aa63e4eaf07f36465b2174e516fbc119
Andreas Niedermair <andreas@niedermair.name>
2015-05-21 23:33:46 +0200

    added demo

--- Chapter 02/DoeInc.Ticketing/request.bat
+++ Chapter 02/DoeInc.Ticketing/request.bat
@@ -1,1 +1,6 @@
 ..\..\Resources\curl.exe -X POST http://localhost:61163/tickets -d @ticket.json --header "Content-Type: application/json" -o ticket.json
+..\..\Resources\curl.exe -X PUT http://localhost:61163/tickets/1 -d @ticket.json --header "Content-Type: application/json" -o ticket.json
+..\..\Resources\curl.exe -X GET http://localhost:61163/tickets --header "Content-Type: application/json"
+..\..\Resources\curl.exe -X POST http://localhost:61163/tickets/1/comments -d @comment.json --header "Content-Type: application/json" -o comment.json
+..\..\Resources\curl.exe -X PUT http://localhost:61163/tickets/1/comments -d @comment.json --header "Content-Type: application/json" -o comment.json
+..\..\Resources\curl.exe -X GET http://localhost:61163/tickets/1/comments --header "Content-Type: application/json"
