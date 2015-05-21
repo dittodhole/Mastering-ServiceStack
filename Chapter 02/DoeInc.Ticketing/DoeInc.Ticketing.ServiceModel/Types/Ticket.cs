d80b1e7f91b3f27b1dce37c2e4c292965b302073
Andreas Niedermair <andreas@niedermair.name>
2015-05-21 23:29:27 +0200

    removed user information from entities

--- Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.ServiceModel/Types/Ticket.cs
+++ Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.ServiceModel/Types/Ticket.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace DoeInc.Ticketing.ServiceModel.Types
+﻿namespace DoeInc.Ticketing.ServiceModel.Types
 {
     public class Ticket : EntityBase
     {
         public string Title { get; set; }
         public Status Status { get; set; }
-        public string Creator { get; set; }
-        public string Processor { get; set; }
     }
 }
