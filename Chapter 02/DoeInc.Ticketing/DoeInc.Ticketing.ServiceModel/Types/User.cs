97cee9cc9e0f1986ebfcc74bb3718eda7ce84a4a
Andreas Niedermair <andreas@niedermair.name>
2015-05-21 23:20:22 +0200

    removed user

--- Chapter 02/DoeInc.Ticketing/DoeInc.Ticketing.ServiceModel/Types/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DoeInc.Ticketing.ServiceModel.Types
-{
-    public class User : EntityBase
-    {
-        public string Login { get; set; }
-        public string PasswordHash { get; set; }
-        public string EmailAdddress { get; set; }
-    }
-}
