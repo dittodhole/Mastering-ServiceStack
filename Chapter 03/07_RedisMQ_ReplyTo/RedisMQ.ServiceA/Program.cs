b215e9cc12ab3dee78d6f02da5bbd1fcd68b2ac2
Andreas Niedermair <andreas@niedermair.name>
2015-06-13 23:14:05 +0200

    added example RedisMQ_ReplyTo

---
+++ Chapter 03/07_RedisMQ_ReplyTo/RedisMQ.ServiceA/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RedisMQ.Models;
+using ServiceStack.Messaging;
+using ServiceStack.Messaging.Redis;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+
+namespace RedisMQ.ServiceA
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var redisClientManager = new BasicRedisClientManager();
+            var redisMqServer = new RedisMqServer(redisClientManager);
+            var messageQueueClient = redisMqServer.CreateMessageQueueClient();
+
+            for (var i = 0;
+                 i < 10;
+                 i++)
+            {
+                var queueName = QueueNames.GetTempQueueName();
+                var hello = new Hello
+                            {
+                                Name = i.ToString()
+                            };
+                var message = new Message<Hello>(hello)
+                              {
+                                  ReplyTo = queueName
+                              };
+                messageQueueClient.Publish(message);
+                var response = messageQueueClient.Get<HelloResponse>(queueName);
+                var helloResponse = response.GetBody();
+                helloResponse.Result.Print();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
