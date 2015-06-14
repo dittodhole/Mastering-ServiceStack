89e039bdb8b0bdfdf12277653f34f289bcc88a4d
Andreas Niedermair <andreas@niedermair.name>
2015-06-14 22:32:52 +0200

    added ServerEvents_ServerSideEvents

---
+++ Chapter 03/18_ServerEvents_ServerSideEvents/ServerEvents.Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace ServerEvents.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serverEventsClient = new ServerEventsClient("http://localhost:54392");
+            serverEventsClient.OnMessage = serverEventMessage =>
+                                           {
+                                               serverEventMessage.PrintDump();
+                                           };
+            serverEventsClient.OnCommand = serverEventMessage =>
+                                           {
+                                               serverEventMessage.PrintDump();
+                                           };
+            serverEventsClient.OnConnect = serverEventMessage =>
+                                           {
+                                               serverEventMessage.PrintDump();
+                                           };
+            serverEventsClient.OnException = exception =>
+                                             {
+                                                 exception.PrintDump();
+                                             };
+            serverEventsClient.OnHeartbeat = () =>
+                                             {
+                                                 "heartbeat".Print();
+                                             };
+            serverEventsClient.Connect();
+
+            "listening to the server".Print();
+
+            Console.ReadLine();
+        }
+    }
+}
