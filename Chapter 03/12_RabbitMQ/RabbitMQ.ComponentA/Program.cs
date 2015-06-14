a23fe00c9b42af1e1562be06e7554e2b7dd651d4
Andreas Niedermair <andreas@niedermair.name>
2015-06-14 14:50:07 +0200

    added example RabbitMQ

---
+++ Chapter 03/12_RabbitMQ/RabbitMQ.ComponentA/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Models;
+using ServiceStack;
+using ServiceStack.RabbitMq;
+
+namespace RabbitMQ.ComponentA
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rabbitMqServer = new RabbitMqServer();
+            var messageProducer = rabbitMqServer.CreateMessageProducer();
+
+            for (var i = 0;
+                 i < 10;
+                 i++)
+            {
+                var hello = new Hello
+                            {
+                                Name = i.ToString()
+                            };
+                messageProducer.Publish(hello);
+            }
+
+            messageProducer.Dispose();
+            rabbitMqServer.Dispose();
+        }
+    }
+}
---
+++ Chapter 03/12_RabbitMQ/RabbitMQ.ComponentB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Models;
+using ServiceStack;
+using ServiceStack.RabbitMq;
+using ServiceStack.Text;
+
+namespace RabbitMQ.ComponentB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rabbitMqServer = new RabbitMqServer();
+            rabbitMqServer.RegisterHandler<Hello>(message =>
+                                                  {
+                                                      var hello = message.GetBody();
+                                                      var name = hello.Name;
+                                                      var result = "Hello {0}".Fmt(name);
+
+                                                      result.Print();
+
+                                                      return null;
+                                                  });
+            rabbitMqServer.Start();
+
+            "listening for hello messages".Print();
+
+            Console.ReadLine();
+
+            rabbitMqServer.Dispose();
+        }
+    }
+}
