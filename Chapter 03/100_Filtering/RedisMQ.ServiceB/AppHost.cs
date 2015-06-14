4a44fee592e8cafd319e014fcf6dd2fb47cc542f
Andreas Niedermair <andreas@niedermair.name>
2015-06-14 13:02:10 +0200

    added example for filtering

---
+++ Chapter 03/100_Filtering/RedisMQ.ServiceB/AppHost.cs
@@ -0,0 +1,43 @@
+﻿using Funq;
+using RedisMQ.Models;
+using ServiceStack;
+using ServiceStack.Messaging.Redis;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+
+namespace RedisMQ.ServiceB
+{
+    public class AppHost : AppSelfHostBase
+    {
+        public AppHost()
+            : base("Hello Service",
+                   typeof (Service).Assembly)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            this.Routes.Add<Hello>("/hello");
+            this.Routes.Add<Hello>("/hello/{Name}");
+
+            this.RegisterTypedMessageRequestFilter<Hello>((request,
+                                                           response,
+                                                           dto) =>
+                                                          {
+                                                              "request coming in".Print();
+                                                          });
+            this.RegisterTypedMessageResponseFilter<HelloResponse>((request,
+                                                                    response,
+                                                                    dto) =>
+                                                                   {
+                                                                       "response going out".Print();
+                                                                   });
+
+            container.Register<IRedisClientsManager>(arg => new RedisManagerPool());
+            var redisClientsManager = container.Resolve<IRedisClientsManager>();
+            var redisMqServer = new RedisMqServer(redisClientsManager);
+            redisMqServer.RegisterHandler<Hello>(this.ServiceController.ExecuteMessage);
+            redisMqServer.Start();
+        }
+    }
+}
---
+++ Chapter 03/100_Filtering/RedisMQ.ServiceB/Service.cs
@@ -0,0 +1,20 @@
+﻿using RedisMQ.Models;
+using ServiceStack;
+
+namespace RedisMQ.ServiceB
+{
+    public class Service : ServiceStack.Service,
+                           IAny<Hello>
+    {
+        public object Any(Hello request)
+        {
+            var name = request.Name;
+            var helloResponse = new HelloResponse
+                                {
+                                    Result = "Hello {0}".Fmt(name)
+                                };
+
+            return helloResponse;
+        }
+    }
+}
