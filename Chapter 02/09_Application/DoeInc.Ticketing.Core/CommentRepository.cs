b62ee5f0a388a4b7de0c918afb60be70cf70c7d7
Andreas Niedermair <andreas@niedermair.name>
2015-06-22 22:27:16 +0200

    init chapter 02 application (final)

---
+++ Chapter 02/09_Application/DoeInc.Ticketing.Core/CommentRepository.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using DoeInc.Ticketing.ServiceModel.Types;
+using ServiceStack;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+
+namespace DoeInc.Ticketing.Core
+{
+    public class CommentRepository : IRepository
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public CommentRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            this._dbConnectionFactory = dbConnectionFactory;
+        }
+
+        private IDbConnectionFactory ConnectionFactory
+        {
+            get
+            {
+                return this._dbConnectionFactory;
+            }
+        }
+
+        public void Initialize()
+        {
+            using (var db = this.ConnectionFactory.Open())
+            {
+                db.CreateTableIfNotExists<Comment>();
+            }
+        }
+
+        public bool Delete(int ticketId,
+                           int commentId,
+                           string userAuthId)
+        {
+            using (var db = this.ConnectionFactory.Open())
+            {
+                return db.Delete<Comment>(new
+                                          {
+                                              Id = commentId,
+                                              TicketId = ticketId,
+                                              CreatorUserAuthId = userAuthId
+                                          }) > 0;
+            }
+        }
+
+        public List<Comment> Read(int ticketId)
+        {
+            using (var db = this.ConnectionFactory.Open())
+            {
+                return db.Select<Comment>(arg => arg.TicketId == ticketId);
+            }
+        }
+
+        public Comment Store(Comment comment)
+        {
+            bool success;
+
+            using (var db = this.ConnectionFactory.Open())
+            {
+                if (comment.Id <= 0)
+                {
+                    success = db.Save(comment);
+                }
+                else
+                {
+                    // this is need for not loosing properties of initial storing (eg creator)
+                    comment.PopulateWith(db.SingleById<Comment>(comment.Id));
+
+                    success = db.Update(comment) == 1;
+                    if (success)
+                    {
+                        comment = db.SingleById<Comment>(comment.Id);
+                    }
+                }
+            }
+
+            if (success)
+            {
+                return comment;
+            }
+            return null;
+        }
+    }
+}
---
+++ Chapter 02/09_Application/DoeInc.Ticketing.ServiceInterface/CommentService.cs
@@ -0,0 +1,77 @@
+﻿using DoeInc.Ticketing.Core;
+using DoeInc.Ticketing.ServiceModel;
+using DoeInc.Ticketing.ServiceModel.Types;
+using ServiceStack;
+
+namespace DoeInc.Ticketing.ServiceInterface
+{
+    [Authenticate]
+    public class CommentService : Service,
+                                  IGet<GetComments>,
+                                  IPost<StoreComment>,
+                                  IPut<StoreComment>,
+                                  IDeleteVoid<DeleteComment>
+    {
+        private readonly CommentRepository _commentRepository;
+
+        public CommentService(CommentRepository commentRepository)
+        {
+            this._commentRepository = commentRepository;
+        }
+
+        private CommentRepository Repository
+        {
+            get
+            {
+                return this._commentRepository;
+            }
+        }
+
+        public void Delete(DeleteComment request)
+        {
+            var ticketId = request.TicketId;
+            var commentId = request.Id;
+            var userAuthId = this.GetSession()
+                                 .UserAuthId;
+
+            if (this.Repository.Delete(ticketId,
+                                       commentId,
+                                       userAuthId))
+            {
+                this.Request.RemoveFromCache(this.Cache,
+                                             UrnId.Create<GetComments>(ticketId));
+            }
+        }
+
+        public object Get(GetComments request)
+        {
+            var ticketId = request.TicketId;
+
+            return this.Request.ToOptimizedResultUsingCache(this.Cache,
+                                                            UrnId.Create<GetComments>(ticketId),
+                                                            () => this.Repository.Read(ticketId));
+        }
+
+        public object Post(StoreComment request)
+        {
+            var ticketId = request.TicketId;
+            var comment = request.ConvertTo<Comment>();
+
+            this.Request.RemoveFromCache(this.Cache,
+                                         UrnId.Create<GetComments>(ticketId));
+            return this.Repository.Store(comment);
+        }
+
+        public object Put(StoreComment request)
+        {
+            var ticketId = request.TicketId;
+            var comment = request.ConvertTo<Comment>();
+            comment.CreatorUserAuthId = this.GetSession()
+                                            .UserAuthId;
+
+            this.Request.RemoveFromCache(this.Cache,
+                                         UrnId.Create<GetComments>(ticketId));
+            return this.Repository.Store(comment);
+        }
+    }
+}
