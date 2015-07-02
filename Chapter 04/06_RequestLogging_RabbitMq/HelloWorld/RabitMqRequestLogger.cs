e1b8367b52da41c85d4002029e4ac6b4b158b338
Andreas Niedermair <andreas@niedermair.name>
2015-07-03 00:56:16 +0200

    added example for request logging with RabbitMq

---
+++ Chapter 04/06_RequestLogging_RabbitMq/HelloWorld/RabitMqRequestLogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack;
+using ServiceStack.Host;
+using ServiceStack.RabbitMq;
+using ServiceStack.Web;
+
+namespace HelloWorld
+{
+    public class RabitMqRequestLogger : InMemoryRollingRequestLogger
+    {
+        public override void Log(IRequest request,
+                                 object requestDto,
+                                 object response,
+                                 TimeSpan requestDuration)
+        {
+            Type requestType;
+            if (requestDto == null)
+            {
+                requestType = null;
+            }
+            else
+            {
+                requestType = requestDto.GetType();
+            }
+
+            if (this.ExcludeRequestType(requestType))
+            {
+                return;
+            }
+
+            var requestLogEntry = this.CreateEntry(request,
+                                                   requestDto,
+                                                   response,
+                                                   requestDuration,
+                                                   requestType);
+
+            requestLogEntry.Items.Add("Component",
+                                      "HelloWorld");
+
+            using (var rabbitMqServer = new RabbitMqServer())
+            {
+                using (var messageProducer = rabbitMqServer.CreateMessageProducer())
+                {
+                    messageProducer.Publish(requestLogEntry);
+                }
+            }
+        }
+
+        public override List<RequestLogEntry> GetLatestLogs(int? take)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Chapter 04/06_RequestLogging_RabbitMq/LogReceiver/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.RabbitMq;
+using ServiceStack.Text;
+
+namespace LogReceiver
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var rabbitMqServer = new RabbitMqServer())
+            {
+                rabbitMqServer.RegisterHandler<RequestLogEntry>(message =>
+                                                                {
+                                                                    var requestLogEntry = message.GetBody();
+
+                                                                    requestLogEntry.PrintDump();
+
+                                                                    return null;
+                                                                });
+                rabbitMqServer.Start();
+
+                "Press Enter to exit".Print();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
