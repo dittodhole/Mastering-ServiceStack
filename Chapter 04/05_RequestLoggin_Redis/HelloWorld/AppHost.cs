b0e4da2561dacbb2db1ff4044d0f6cbde820c486
Andreas Niedermair <andreas@niedermair.name>
2015-07-02 23:22:13 +0200

    added example for logging with redis

---
+++ Chapter 04/05_RequestLoggin_Redis/HelloWorld/AppHost.cs
@@ -0,0 +1,26 @@
+﻿using Funq;
+using ServiceStack;
+using ServiceStack.Host;
+using ServiceStack.Redis;
+
+namespace HelloWorld
+{
+    public class AppHost : AppSelfHostBase
+    {
+        public AppHost()
+            : base("Hello World",
+                   typeof (HelloService).Assembly)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            container.Register<IRedisClientsManager>(arg => new PooledRedisClientManager());
+
+            this.Plugins.Add(new RequestLogsFeature
+                             {
+                                 RequestLogger = new RedisRequestLogger(container.Resolve<IRedisClientsManager>())
+                             });
+        }
+    }
+}
