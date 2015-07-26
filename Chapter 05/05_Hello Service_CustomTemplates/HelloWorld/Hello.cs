f33820de4d532bcac1b5e35db75d069654c8c81c
Andreas Niedermair <andreas@niedermair.name>
2015-07-26 20:08:52 +0200

    added example for custom templating metadata

---
+++ Chapter 05/05_Hello Service_CustomTemplates/HelloWorld/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.DataAnnotations;
+
+namespace HelloWorld
+{
+    [Route("/hello/{Name}")]
+    [Api("foobar")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        [Obsolete]
+        public string Name { get; set; }
+        public Volume Volume { get; set; }
+    }
+}
