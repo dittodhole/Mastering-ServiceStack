4f4db5313975277e706436acefdc5ac22686310b
Andreas Niedermair <andreas@niedermair.name>
2015-07-26 02:49:37 +0200

    adaptions to examples

--- Chapter 05/03_Hello Service_Annotations/HelloWorld/Hello.cs
+++ Chapter 05/03_Hello Service_Annotations/HelloWorld/Hello.cs
@@ -3,12 +3,15 @@ using ServiceStack.DataAnnotations;
 
 namespace HelloWorld
 {
-    [Route("/hello/{Name}")]
-    [Exclude(Feature.Metadata)]
-    [Restrict(VisibleLocalhostOnly = true)]
+    [Api("Endpoint to greet a person.")]
+    [Route("/hello/{Name}", Summary = "Greets a person.")]
     public class Hello : IReturn<HelloResponse>
     {
+        [ApiMember(IsRequired = true, Description = "Defines the person to greet.")]
         public string Name { get; set; }
+
+        [ApiMember(IsRequired = true, Description = "Defines the volume of the greeting.")]
+        [ApiAllowableValues("Volume", typeof (Volume))]
         public Volume Volume { get; set; }
     }
 }
--- Chapter 05/04_Hello Service_RestrictDocumentation/HelloWorld/Hello.cs
+++ Chapter 05/04_Hello Service_RestrictDocumentation/HelloWorld/Hello.cs
@@ -3,16 +3,12 @@ using ServiceStack.DataAnnotations;
 
 namespace HelloWorld
 {
-    [Api("Endpoint to greet a person.")]
-    [Route("/hello/{Name}", Summary = "Greets a person.")]
-    //[Exclude(Feature.Metadata)]
+    [Route("/hello/{Name}")]
+    [Exclude(Feature.Metadata)]
+    [Restrict(VisibleLocalhostOnly = true)]
     public class Hello : IReturn<HelloResponse>
     {
-        [ApiMember(IsRequired = true, Description = "Defines the person to greet.", AllowMultiple = true)]
         public string Name { get; set; }
-
-        [ApiMember(IsRequired = true, Description = "Defines the volume of the greeting.")]
-        [ApiAllowableValues("Volume", typeof (Volume))]
         public Volume Volume { get; set; }
     }
 }
