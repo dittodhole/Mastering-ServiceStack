f12420b75e7e3fe3c07389f20693d31a80b396ee
Andreas Niedermair <andreas@niedermair.name>
2015-10-18 19:20:39 +0200

    added IHasRequestIdentifier interface on HelloResponse

--- Chapter 06/01_Hello Service_RequestIdentifier/HelloWorld/HelloResponse.cs
+++ Chapter 06/01_Hello Service_RequestIdentifier/HelloWorld/HelloResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace HelloWorld
+﻿using DoeInc.ServiceStack.Extensions;
+
+namespace HelloWorld
 {
-    public class HelloResponse
+    public class HelloResponse : IHasRequestIdentifier
     {
         public string Result { get; set; }
+        public string RequestIdentifier { get; set; }
     }
 }
