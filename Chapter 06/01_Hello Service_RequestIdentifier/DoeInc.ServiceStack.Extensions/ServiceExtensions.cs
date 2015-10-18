278374965ed1366879ee74e61547b30c79219815
Andreas Niedermair <andreas@niedermair.name>
2015-10-18 19:20:26 +0200

    added factory-method for creating requests
    this should be used for sending requests from a service to a MQ

---
+++ Chapter 06/01_Hello Service_RequestIdentifier/DoeInc.ServiceStack.Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack;
+
+namespace DoeInc.ServiceStack.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static T Create<T>(this Service service) where T : IHasRequestIdentifier, new()
+        {
+            var requestIdentifier = service.Request.GetRequestIdentifier();
+            var instance = new T
+                           {
+                               RequestIdentifier = requestIdentifier
+                           };
+
+            return instance;
+        }
+    }
+}
