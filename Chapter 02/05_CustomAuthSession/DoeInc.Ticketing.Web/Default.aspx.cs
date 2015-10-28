2090f4577a63e70fe97023dd54e059e99f38867e
Andreas Niedermair <andreas@niedermair.name>
2015-10-28 19:46:10 +0100

    cleanup

--- Chapter 02/05_CustomAuthSession/DoeInc.Ticketing.Web/Default.aspx.cs
+++ Chapter 02/05_CustomAuthSession/DoeInc.Ticketing.Web/Default.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using ServiceStack.AspNet;
 
 namespace DoeInc.Ticketing.Web
